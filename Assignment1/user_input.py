fb264509eed909a68a97a604a5a3e0c4b82cf4ee
Astro-Kid <bupechiyana15@gmil.com>
2024-04-10 15:01:48 +0200

    Power Learn Assignment1 (py)

---
+++ Assignment1/user_input.py
@@ -0,0 +1,4 @@
+name = str(input("What is your name ? "))
+age = int(input("How old are ? "))
+location = str(input("What is your current location ? "))
+print("Hello", name, ",you are", age, "years old,", "you are in", location)
